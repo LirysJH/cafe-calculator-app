a1f1fc986ae795eb99c8452ca11a370651156a12
LirysJH <48212972+LirysJH@users.noreply.github.com>
2020-06-07 15:36:01 +0300

    Deleted Calculator component and moved it's code to App; added switching between 'by tasks' & 'by cups'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
 import React, {Component} from 'react';
-import './App.css';
-import Calculator from './Components/Calculator/Calculator';
+import './css/App.css';
+import Citizens from './Components/Citizens/Citizens';
 import Loader from './Components/Loader/Loader';
 
 class App extends Component {
-  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasksState: false,
+      cupsState: false,
+      /* averageState: false,
+      averageButtonBackground: ""  average = 50/50 */
+      tasksButtonBackground: "",
+      cupsButtonBackground: ""      
+    }
+  };
+
+  toggleTasksState = () => {
+    this.setState({
+      tasksState: !this.state.tasksState,
+      cupsState: false
+    });
+
+    if(this.state.tasksState) {
+      this.setState({
+        tasksButtonBackground: ""
+      });
+    }
+    else {
+      this.setState({
+        tasksButtonBackground: "hsl(24, 100%, 41%)",
+        cupsButtonBackground: "" 
+      });
+    }
+  }
+
+  toggleCupsState = () => {
+    this.setState({
+      tasksState: false,
+      cupsState: !this.state.cupsState
+    });
+
+    if(this.state.cupsState) {
+      this.setState({
+        cupsButtonBackground: ""
+      });
+    }
+    else {
+      this.setState({
+        tasksButtonBackground: "",
+        cupsButtonBackground: "hsl(24, 100%, 41%)"
+      });
+    }
+  }
+
   render() {
 
     return(
@@ -40,7 +90,41 @@ class App extends Component {
           - day/night mode switch button
         */}
         <hr />
-        <Calculator />
+        
+        <section>
+        <p>How would you like to calculate awards?</p>
+        <div className="buttonsBox">
+          <button className="buttonsBox__button button-tasks"
+                  onClick={this.toggleTasksState}
+                  style={{backgroundColor: this.state.tasksButtonBackground}}>
+            BY TASKS
+          </button>
+          <button className="buttonsBox__button button-cups"
+                  onClick={this.toggleCupsState}
+                  style={{backgroundColor: this.state.cupsButtonBackground}} >
+            BY CUPS
+          </button>
+          {/*
+          <button className="buttonsBox__button button-50-50">
+            50/50
+          </button>
+          */}
+        </div>
+      </section>
+      <hr />
+      <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+      <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+      
+      <hr />
+      { this.state.tasksState && (
+        <Citizens tasksState={this.state.tasksState}
+                  cupsState={this.state.cupsState} />
+      )}
+
+      { this.state.cupsState && (
+        <Citizens tasksState={this.state.tasksState}
+                  cupsState={this.state.cupsState} />
+      )}
       </div>
     )
   };
--- src/Components/Citizens/Citizens.jsx
+++ src/Components/Citizens/Citizens.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import './css/Citizens.css';
 
 const Citizens = (props) => {
+  let {tasksState, cupsState}={...props};
+
   return (
     <div className="Citizens">
       <div className="Citizens__container">
         <div className="Citizens__container--block">
           <div className="box label-box">
-            <label className="label-ct label-ct-1" htmlFor="ct-1">Citizen 1:</label>
+            <label className="label-ct label-ct-1" htmlFor="ct-1">Citizen 1:</label>            
+          </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
           </div>
           <div className="box input-box">
             <input className="citiz ct-1" type="number" />
@@ -30,6 +42,16 @@ const Citizens = (props) => {
           <div className="box label-box">
             <label className="label-ct label-ct-2" htmlFor="ct-2">Citizen 2:</label>
           </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
+          </div>
           <div className="box input-box">
             <input className="citiz ct-2" type="number" />
           </div>
@@ -48,6 +70,16 @@ const Citizens = (props) => {
         <div className="Citizens__container--block">
           <div className="box label-box">
             <label className="label-ct label-ct-3" htmlFor="ct-3">Citizen 3:</label>
+          </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
           </div>
           <div className="box input-box">
             <input className="citiz ct-3" type="number" />
@@ -68,6 +100,16 @@ const Citizens = (props) => {
           <div className="box label-box">
             <label className="label-ct label-ct-4" htmlFor="ct-4">Citizen 4:</label>
           </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
+          </div>
           <div className="box input-box">
             <input className="citiz ct-4" type="number" />
           </div>
@@ -86,6 +128,16 @@ const Citizens = (props) => {
         <div className="Citizens__container--block">
           <div className="box label-box">
             <label className="label-ct label-ct-5" htmlFor="ct-5">Citizen 5:</label>
+          </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
           </div>
           <div className="box input-box">
             <input className="citiz ct-5" type="number" />
@@ -106,6 +158,16 @@ const Citizens = (props) => {
           <div className="box label-box">
             <label className="label-ct label-ct-6" htmlFor="ct-6">Citizen 6:</label>
           </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
+          </div>
           <div className="box input-box">
             <input className="citiz ct-6" type="number" />
           </div>
@@ -124,6 +186,16 @@ const Citizens = (props) => {
         <div className="Citizens__container--block">
           <div className="box label-box">
             <label className="label-ct label-ct-7" htmlFor="ct-7">Citizen 7:</label>
+          </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
           </div>
           <div className="box input-box">
             <input className="citiz ct-7" type="numbers" />
@@ -144,6 +216,16 @@ const Citizens = (props) => {
           <div className="box label-box">
             <label className="label-ct label-ct-8" htmlFor="ct-8">Citizen 8:</label>
           </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
+          </div>
           <div className="box input-box">
             <input className="citiz ct-8" type="number" />
           </div>
@@ -162,6 +244,16 @@ const Citizens = (props) => {
         <div className="Citizens__container--block">
           <div className="box label-box">
             <label className="label-ct label-ct-9" htmlFor="ct-9">Citizen 9:</label>
+          </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
           </div>
           <div className="box input-box">
             <input className="citiz ct-9" type="number" />
@@ -182,6 +274,16 @@ const Citizens = (props) => {
           <div className="box label-box">
             <label className="label-ct label-ct-10" htmlFor="ct-10">Citizen 10:</label>
           </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
+          </div>
           <div className="box input-box">
             <input className="citiz ct-10" type="number" />
           </div>
@@ -200,6 +302,16 @@ const Citizens = (props) => {
         <div className="Citizens__container--block">
           <div className="box label-box">
             <label className="label-ct label-ct-11" htmlFor="ct-11">Citizen 11:</label>
+          </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
           </div>
           <div className="box input-box">
             <input className="citiz ct-11" type="number" />
@@ -220,6 +332,16 @@ const Citizens = (props) => {
           <div className="box label-box">
             <label className="label-ct label-ct-12" htmlFor="ct-12">Citizen 12:</label>
           </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
+          </div>
           <div className="box input-box">
             <input className="citiz ct-12" type="number" />
           </div>
@@ -238,6 +360,16 @@ const Citizens = (props) => {
         <div className="Citizens__container--block">
           <div className="box label-box">
             <label className="label-ct label-ct-13" htmlFor="ct-13">Citizen 13:</label>
+          </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
           </div>
           <div className="box input-box">
             <input className="citiz ct-13" type="number" />
@@ -258,6 +390,16 @@ const Citizens = (props) => {
           <div className="box label-box">
             <label className="label-ct label-ct label-ct-14" htmlFor="ct-14">Citizen 14:</label>
           </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
+          </div>
           <div className="box input-box">
             <input className="citiz ct-14" type="number" />
           </div>
@@ -276,6 +418,16 @@ const Citizens = (props) => {
         <div className="Citizens__container--block">
           <div className="box label-box">
             <label className="label-ct label-ct-15" htmlFor="ct-15">Citizen 15:</label>
+          </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
           </div>
           <div className="box input-box">
             <input className="citiz ct-15" type="number" />
@@ -296,6 +448,16 @@ const Citizens = (props) => {
           <div className="box label-box">
             <label className="label-ct label-ct-16" htmlFor="ct-16">Citizen 16:</label>
           </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
+          </div>
           <div className="box input-box">
             <input className="citiz ct-16" type="number" />
           </div>
@@ -314,6 +476,16 @@ const Citizens = (props) => {
         <div className="Citizens__container--block">
           <div className="box label-box">
             <label className="label-ct label-ct-17" htmlFor="ct-17">Citizen 17:</label>
+          </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
           </div>
           <div className="box input-box">
             <input className="citiz ct-17" type="number" />
@@ -334,6 +506,16 @@ const Citizens = (props) => {
           <div className="box label-box">
             <label className="label-ct label-ct-18" htmlFor="ct-18">Citizen 18:</label>
           </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
+          </div>
           <div className="box input-box">
             <input className="citiz ct-18" type="number" />
           </div>
@@ -353,6 +535,16 @@ const Citizens = (props) => {
           <div className="box label-box">
             <label className="label-ct label-ct-19" htmlFor="ct-19">Citizen 19:</label>
           </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
+          </div>
           <div className="box input-box">
             <input className="citiz ct-19" type="number" />
           </div>
@@ -371,6 +563,16 @@ const Citizens = (props) => {
         <div className="Citizens__container--block">
           <div className="box label-box">
             <label className="label-ct label-ct-20" htmlFor="ct-20">Citizen 20:</label>
+          </div>
+          <div className="box icon-box">
+            {cupsState &&
+                (
+                  <img className="icon icon-cup" src="https://i.imgur.com/ZlvUter.png" alt="cup" />
+              )}
+            {tasksState &&
+                (
+                  <img className="icon icon-task" src="https://i.imgur.com/JPaeM6P.png" alt="task" />
+              )}
           </div>
           <div className="box input-box">
             <input className="citiz ct-20" type="number" />
