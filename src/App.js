e22e9c25d18ef0790d9ed20b63fd99defb0d8d51
LirysJH <48212972+LirysJH@users.noreply.github.com>
2020-06-23 21:49:28 +0300

    Added back to top smooth nagivation button; working on changing citizens' input values

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, {Component} from 'react';
 import './css/App.css';
 import Citizens from './Components/Citizens/Citizens';
@@ -16,92 +17,132 @@ class App extends Component {
       tasksButtonBackground: "",
       cupsButtonBackground: "",
       data: {
-        generalAmount: 0,
-        generalDiamondsAmount: 0,
-        generalRubiesAmount: 0
-      },
-      citizensData: [
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-        {
-          diamsEarned: 0,
-          rubiesEarned: 0,
-        },
-      ]
+        generalAmount: 64000,
+        generalDiamondsAmount: 350,
+        generalRubiesAmount: 70,
+        citizensData: [
+          {
+            id: 1,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 2500
+          },
+          {
+            id: 2,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 3,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 4,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 5,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 6,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 7,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 8,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 9,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 10,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 11,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 12,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 13,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 14,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 15,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 16,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 17,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 18,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 19,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          },
+          {
+            id: 20,
+            diamsEarned: 0,
+            rubiesEarned: 0,
+            personalResult: 0
+          }
+        ]
+      }
     }
   };
 
@@ -170,6 +211,20 @@ class App extends Component {
     });
   }
 
+  changeValueHandle = (id, key, value) => {
+    this.setState({
+      data:
+      {
+        citizensData: this.state.data.citizensData.map(
+          (item) =>
+            item.id === id
+              ? { ...item, [key]: value }
+              : item
+        )
+      }
+    });
+  }
+
   render() {
 
     return(
@@ -177,6 +232,13 @@ class App extends Component {
         {/*
           <Loader />
         */}
+
+        <a href="#"
+            className="backToTopLink"
+            title="Click to get to the top" >
+              <i className="fas fa-arrow-up"></i>
+        </a>
+        
         <header>
           <h3>Attention! Non-commercial usage only</h3>
           <p>
@@ -185,7 +247,7 @@ class App extends Component {
             <a className="mycafeLink" href="https://mycafe.space"
               target="_blank"
               rel="noopener noreferrer">
-                Online Calculator
+                Melsoft Games Online Calculator
             </a>
             &nbsp;
             made by
@@ -205,7 +267,8 @@ class App extends Component {
           TODO:
           - smooth top navigation 
           - language switch button
-          - day/night mode switch button ? necessary ?
+          - correct mathematical calculations
+          - fix icons near to general amount input field
         */}
         
         <main>
@@ -251,13 +314,17 @@ class App extends Component {
             { this.state.tasksState && (
               <Citizens tasksState={this.state.tasksState}
                         cupsState={this.state.cupsState}
-                        trophies={this.state.citizensData} />
+                        trophies={this.state.data.citizensData} />
             )}
 
             { this.state.cupsState && (
               <Citizens tasksState={this.state.tasksState}
                         cupsState={this.state.cupsState}
-                        trophies={this.state.citizensData} />
+                        trophies={this.state.data.citizensData}
+                        generalAmount={this.state.data.generalAmount}
+                        generalDiamondsAmount={this.state.data.generalDiamondsAmount}
+                        generalRubiesAmount={this.state.data.generalRubiesAmount}
+                        onChangeValueHandle={this.changeValueHandle} />
             )}
           </section>
         </main>
--- src/Components/Citizens/Citizens.jsx
+++ src/Components/Citizens/Citizens.jsx
@@ -5,17 +5,47 @@ const Citizens = (props) => {
   let {
         tasksState,
         cupsState,
-        trophies
+        trophies,
+        generalAmount,
+        generalDiamondsAmount,
+        generalRubiesAmount
       } = {...props};
 
   let CitizensList = [];
 
   for (let i=0; i<20; i++) {
 
-    let classValue = `label-ct label-ct-${i+1}`;
+    let percentage = 0;
+
+    console.log("trophies[i].personalResult - ", trophies[i].personalResult);
+    
+    if(trophies[i].personalResult > 0 && trophies[i].personalResult <= generalAmount) {
+      percentage = +Math.floor(trophies[i].personalResult*100/generalAmount);
+    }
+    else percentage = 0;
+      
+    console.log("percentage - ", percentage);
+    
+    if ((percentage > 0 && percentage <= 100)
+        && (trophies[i].personalResult > 0 && trophies[i].personalResult <= generalAmount)) {
+      trophies[i].diamsEarned = +Math.floor(generalDiamondsAmount*percentage/100);
+    }
+    else trophies[i].diamsEarned = 0;
+
+    if ((percentage > 0 && percentage <= 100)
+        && (trophies[i].personalResult > 0 && trophies[i].personalResult <= generalAmount)) {
+      trophies[i].rubiesEarned = +Math.floor(generalRubiesAmount*percentage/100);
+    }
+    else trophies[i].rubiesEarned = 0;
+
+    console.log("trophies[i].diamsEarned - ", trophies[i].diamsEarned);
+    console.log("trophies[i].rubiesEarned - ", trophies[i].rubiesEarned);
+
+    const keyBlockValue = `citizens-block-${i+1}`;
+    const classValue = `label-ct label-ct-${i+1}`;
     
     CitizensList.push(
-      <div className="Citizens__container--block">
+      <div className="Citizens__container--block" key={keyBlockValue}>
         <div className="box label-box">
           <label className={classValue} htmlFor="citiz">Citizen {i+1}:</label>            
         </div>
@@ -30,18 +60,21 @@ const Citizens = (props) => {
             )}
         </div>
         <div className="box input-box">
-          <input className="citiz" type="number" />
+          <input className="citiz"
+                  type="number"
+                  value={trophies[i].personalResult}
+                  onChange={props.onChangeValueHandle} />
         </div>
         <div className="box text-box">
           <p>Award:</p>
         </div>
         <div className="box diamond-box">
           <i className="fas fa-gem icon icon-diamond"></i>
-          <p>{trophies[i].diamsEarned}</p>
+          <p>{+trophies[i].diamsEarned}</p>
         </div>
         <div className="box ruby-box">
           <i className="fas fa-gem icon icon-ruby"></i>
-          <p>{trophies[i].rubiesEarned}</p>
+          <p >{+trophies[i].rubiesEarned}</p>
         </div>
       </div>
     );
